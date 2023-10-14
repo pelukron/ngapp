3e860ce59715aa6fb7f4aff9e89b34cdfdb03195
Diego Moreno <pelukron@users.noreply.github.com>
2023-10-14 16:03:08 -0600

    feat: add reactive form to listen typing user

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs';
+
+type TFormGroup = { search: FormControl<string> };
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
+  private _formBuilder: FormBuilder = inject(FormBuilder)
+
+  searchForm: FormGroup<TFormGroup> = this._formBuilder.nonNullable.group({
+    search: [''],
+  });
+
+  searchValue$ = this.searchForm.controls.search.valueChanges.pipe(
+    debounceTime(200),
+    distinctUntilChanged(),
+  );
 
 }
