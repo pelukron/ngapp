655dac770915539c316d4a873ef6ac01be62a4e5
Diego Moreno <pelukron@users.noreply.github.com>
2023-11-21 22:57:45 -0600

    feat(arch): ng update v17 (#7)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, MenuComponent]
 })
 export class AppComponent {
   title = 'ngapp';
